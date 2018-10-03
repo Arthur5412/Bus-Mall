7cfd6144ac735f73ed6fd86297b30162cf48e4b6
root <root@Arthur.localdomain>
2018-10-03 11:01:03 -0700

    updated

--- app.js
+++ app.js
@@ -32,26 +32,26 @@ function Product(name, filepath, altText) {
 }
 
 //Make new Products instances
-new Product('Bag', 'img/bag.jpg','Bag');
-new Product('Banana Slicer', 'img/banana.jpg','Banana Slicer');
-new Product('Bathroom', 'img/bathroom.jpg','Bathroom');
-new Product('Boots', 'img/boots.jpg','Boots');
-new Product('Breakfast', 'img/breakfast.jpg','Breakfast');
-new Product('Bubblegum', 'img/bubblegum.jpg', 'Bubblegum');
-new Product('Chair', 'img/chair.jpg', 'Chair');
-new Product('Cthulhu', 'img/cthulhu.jpg', 'Cthulhu');
-new Product('Dog Duck', 'img/dog-duck.jpg', 'Dog Duck');
-new Product('Dragon Meat', 'img/dragon.jpg', 'Dragon Meat');
-new Product('Pen', 'img/pen.jpg', 'Pen');
-new Product('Pet Broom', 'img/pet-sweep.jpg', 'Pet Broom');
-new Product('Scissors', 'img/scissors.jpg', 'Scissors');
-new Product('Shark', 'img/shark.jpg', 'Shark');
-new Product('Sweep', 'img/sweep.png', 'Sweep');
-new Product('Tauntaun', 'img/tauntaun.jpg', 'Tauntaun');
-new Product('Unicorn Meat', 'img/unicorn.jpg', 'Unicorn Meat');
-new Product('Tentacle USB', 'img/usb.gif', 'Tentacle USB');
-new Product('Watering Can', 'img/water-can.jpg', 'Watering Can');
-new Product('Wine Glass', 'img/wine-glass.jpg', 'Wine Glass');
+new Product('Bag', 'imgs/bag.jpg','Bag');
+new Product('Banana Slicer', 'imgs/banana.jpg','Banana Slicer');
+new Product('Bathroom', 'imgs/bathroom.jpg','Bathroom');
+new Product('Boots', 'imgs/boots.jpg','Boots');
+new Product('Breakfast', 'imgs/breakfast.jpg','Breakfast');
+new Product('Bubblegum', 'imgs/bubblegum.jpg', 'Bubblegum');
+new Product('Chair', 'imgs/chair.jpg', 'Chair');
+new Product('Cthulhu', 'imgs/cthulhu.jpg', 'Cthulhu');
+new Product('Dog Duck', 'imgs/dog-duck.jpg', 'Dog Duck');
+new Product('Dragon Meat', 'imgs/dragon.jpg', 'Dragon Meat');
+new Product('Pen', 'imgs/pen.jpg', 'Pen');
+new Product('Pet Broom', 'imgs/pet-sweep.jpg', 'Pet Broom');
+new Product('Scissors', 'imgs/scissors.jpg', 'Scissors');
+new Product('Shark', 'imgs/shark.jpg', 'Shark');
+new Product('Sweep', 'imgs/sweep.png', 'Sweep');
+new Product('Tauntaun', 'imgs/tauntaun.jpg', 'Tauntaun');
+new Product('Unicorn Meat', 'imgs/unicorn.jpg', 'Unicorn Meat');
+new Product('Tentacle USB', 'imgs/usb.gif', 'Tentacle USB');
+new Product('Watering Can', 'imgs/water-can.jpg', 'Watering Can');
+new Product('Wine Glass', 'imgs/wine-glass.jpg', 'Wine Glass');
 
 
 //Randomly display products
